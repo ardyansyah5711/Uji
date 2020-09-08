a636af48da823def4803976d6ec39dbc35df2365
Ardyansyah5711 <aardy475@gmail.com>
2020-09-08 13:17:25 +0700

    update

--- app/src/main/java/com/example/uji/MainActivity.java
+++ app/src/main/java/com/example/uji/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.uji;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Uji";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "MyClass.getView() — get item number ");
     }
 }
